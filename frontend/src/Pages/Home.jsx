35be356f15cd65d22d33be967a678a69d951dd90
Mr-Hariom-Kumar <766hariwork@gmail.com>
2025-11-16 03:39:18 +0530

    fixed the env secre key exposure

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -74,7 +74,7 @@ const Home = () => {
           ELECTRICAL
         </p>
       </div>
-      Nothing extra. Just exactly what you asked.
+   
       <div>
         {token ? (
           <DisplayJob />
